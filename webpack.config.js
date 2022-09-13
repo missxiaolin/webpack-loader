69ce4cc83899aaa356161307433c077b6aed7091
lingongbing <462441355@qq.com>
2022-09-13 15:06:49 +0800

    loader 测试

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,50 @@
 // include: path.resolve(__dirname, 'src'),
 
-const path = require("path");
+
+const path = require('path');
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-    ],
-  },
-  resolveLoader: {
-    modules: ["node_modules", "./src/loader"], // node_modules 找loader,如果找不到就 ./src/loader 找
-  },
-  devServer: {
-    contentBase: "./dist",
-    overlay: {
-      warnings: true,
-      errors: true,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    open: true,
-  },
-};
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'text2-loader',
+                        options: {
+                            name: '前端',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'text-loader',
+                        options: {
+                            name: '前端',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolveLoader: {
+        modules: ['node_modules', './src/loader'], // node_modules 找loader,如果找不到就 ./src/loader 找
+    },
+    devServer: {
+        contentBase: './dist',
+        overlay: {
+            warnings: true,
+            errors: true,
+        },
+        open: true,
+    },
+}
